c28a21a71c15289e9f0d7ffcccd26aa99bd0a9e8
Shahed Ahmad <shaheda.a16@gmail.com>
2021-03-21 19:40:32 +0430

    refactor'

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -8,6 +8,7 @@ class Authentication(models.Model):
     token = models.CharField(primary_key=True, max_length=40)
     expiry_date = models.DateTimeField(null=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+    ip = models.CharField(max_length=15)
+    app_version = models.TextField()
 
 
