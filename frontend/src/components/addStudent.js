3f62a45576442e9ab05e49297aacc8c3c6cf414e
Shahed Ahmad <shaheda.a16@gmail.com>
2021-03-26 17:45:58 +0430

    styled

--- frontend/src/components/addStudent.js
+++ frontend/src/components/addStudent.js
@@ -90,7 +90,7 @@ let formData = async function () {
                 <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full border-b border-green-400 py-1 my-2 px-3 focus:outline-none" placeholder= "E-mail"/>
                 {/* <input type="file" onChange={(e) => setFile(e.target.files[0])} /> */}
                 <input type="file" className="w-full border-b border-green-400 py-1 my-2 px-3 focus:outline-none" onChange={(e) => setFile(e.target.files[0])} />
-                <span className="">
+                <span className="flex justify-around">
                     <button onClick={(e) => formData(e.preventDefault())} className="w-1/3 text-white py-1 my-2 px-3 bg-green-600 rounded">Submit</button>
                     <button onClick={(e) => formData(e.preventDefault())} className="w-1/3 text-white py-1 my-2 px-3 bg-red-600 rounded">Back</button>
                 </span>
