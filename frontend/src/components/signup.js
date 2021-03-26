f8568ccb0f0a421eebe5721bc3801c0708bfc2f4
Shahed Ahmad <shaheda.a16@gmail.com>
2021-03-26 18:27:00 +0430

    refacotor

--- frontend/src/components/signup.js
+++ frontend/src/components/signup.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import {useHistory} from 'react-router-dom'
+import {useHistory, Link} from 'react-router-dom'
 
 
 function Signup() {
@@ -38,13 +38,20 @@ function Signup() {
 
 
     return (
-        <div>
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}  placeholder="UserName" required minLength="8" />
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}  placeholder="E-mail" required minLength="8" />
-            <input type="password" value={password1} onChange={(e) => setPassword1(e.target.value)}  placeholder="Password" required minLength="8" />
-            <input type="password" value={password2} onChange={(e) => setPassword2(e.target.value)}  placeholder="Confirm" required minLength="8" />
-            <button type="submit" onClick={(e) => {sign()}}>Submit</button>
-            
+        <div className="bg-gray-100 fixed w-full h-full flex justify-center items-center">
+            <div className="flex flex-col w-1/3 bg-white p-10 rounded">
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}  placeholder="UserName" required className="w-full border-b border-green-400 py-1 my-2 px-3 focus:outline-none"/>
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}  placeholder="E-mail" required className="w-full border-b border-green-400 py-1 my-2 px-3 focus:outline-none"/>
+                <input type="password" value={password1} onChange={(e) => setPassword1(e.target.value)}  placeholder="Password" required className="w-full border-b border-green-400 py-1 my-2 px-3 focus:outline-none"/>
+                <input type="password" value={password2} onChange={(e) => setPassword2(e.target.value)}  placeholder="Confirm" required className="w-full border-b border-green-400 py-1 my-2 px-3 focus:outline-none"/>
+                <button type="submit" onClick={(e) => {sign()}}>Submit</button>
+                <p className="text-center mt-2">
+                    already have an acount?{" "}
+                    <Link to="/login" className="text-blue-600">
+                        Log In
+                    </Link>
+                </p>
+            </div>
         </div>
     )
 }
