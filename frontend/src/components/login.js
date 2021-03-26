d6dfabc3b601a6a56ebd22a69e51eeee99e9ea5d
Shahed Ahmad <shaheda.a16@gmail.com>
2021-03-26 17:57:37 +0430

    refactor

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,79 +1,98 @@
-import React, { useState, useEffect } from 'react'
-import { Redirect, Route, useHistory, Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Redirect, Route, useHistory, Link } from "react-router-dom";
 // import moduleName from 'module'
-import Data from './index'
-import App from '../App'
-
-
+import Data from "./index";
+import App from "../App";
 
 function Login() {
-    let history= useHistory()
-    const [login, setLogin] = useState('')
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    
-    const [IP, setIP] = useState('')
+    let history = useHistory();
+    const [login, setLogin] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [IP, setIP] = useState("");
 
     // useEffect(() => {
     //     fetch('https://json.geoiplookup.io/').then(response => response.json()).then(res => setIP(res.ip))
     // }, [])
 
-    const browserDetails = window.navigator.appVersion
-    console.log(browserDetails)
+    const browserDetails = window.navigator.appVersion;
+    console.log(browserDetails);
 
     // console.log(IP)
     let check = async function (username, password) {
-        if ('192') {
-            const request = new Request('http://localhost:8000/login/', {headers: {'Content-type': 'application/json',}})
+        if ("192") {
+            const request = new Request("http://localhost:8000/login/", {
+                headers: { "Content-type": "application/json" },
+            });
             fetch(request, {
                 method: "POST",
                 body: JSON.stringify({
                     username: username,
                     password: password,
-                    ip : '192.168.1.1',
-                    appVersion: browserDetails
+                    ip: "192.168.1.1",
+                    appVersion: browserDetails,
                 }),
             })
-            .then(response => response.json())
-            .then(res => {if (res.result) {
-                            document.cookie = `token=`;
-                            document.cookie = `username=`;
-                            document.cookie = `ip=`;
-                            alert('Wrong specifications')
-                        } else {
-                            document.cookie = `token=${res.token}`;
-                            document.cookie = `username=${username}`;
-                            document.cookie = `ip=${'192.168.1.1'}`;
-                          look()
-                        }})
-                
+                .then((response) => response.json())
+                .then((res) => {
+                    if (res.result) {
+                        document.cookie = `token=`;
+                        document.cookie = `username=`;
+                        document.cookie = `ip=`;
+                        alert("Wrong specifications");
+                    } else {
+                        document.cookie = `token=${res.token}`;
+                        document.cookie = `username=${username}`;
+                        document.cookie = `ip=${"192.168.1.1"}`;
+                        look();
+                    }
+                });
+
             // .catch(error => console.log(error))
         }
-    }
+    };
     let look = function () {
-        if(document.cookie){
-            history.push('/index');
-            window.location.reload()
+        if (document.cookie) {
+            history.push("/index");
+            window.location.reload();
             // <Data />
             // console.log(document.cookie)
         }
-        }
+    };
 
-         
-    
     return (
         <div className="bg-gray-100 fixed w-full h-full flex justify-center items-center">
             <div className="flex flex-col w-1/3 bg-white p-10 rounded">
-                <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Username" className="w-full border-b border-green-400 py-1 my-2 px-3 focus:outline-none"/>
-                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" className="w-full border-b border-green-400 py-1 my-2 px-3 focus:outline-none"/>
-                <a onClick={() =>check(username, password)} className="w-1/3 text-white py-1 my-4 px-2 bg-green-600 rounded text-center m-auto">Submit</a>
+                <input
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    type="text"
+                    placeholder="Username"
+                    className="w-full border-b border-green-400 py-1 my-2 px-3 focus:outline-none"
+                />
+                <input
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                    placeholder="Password"
+                    className="w-full border-b border-green-400 py-1 my-2 px-3 focus:outline-none"
+                />
+                <a
+                    onClick={() => check(username, password)}
+                    className="w-1/3 text-white py-1 my-4 px-2 bg-green-600 rounded text-center m-auto"
+                >
+                    Submit
+                </a>
                 <p className="text-center mt-2">
-                    or create acount, <Link to="/signup" className="text-blue-600">SignUP</Link>
-                </p>           
+                    or create acount,{" "}
+                    <Link to="/signup" className="text-blue-600">
+                        SignUP
+                    </Link>
+                </p>
             </div>
-                
         </div>
-    )
+    );
 }
 
-export default Login
+export default Login;
