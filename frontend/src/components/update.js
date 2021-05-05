be2fdf7e0f2541b438787061c4cfda3e655f9d70
Shahed Ahmad <shaheda.a16@gmail.com>
2021-05-05 20:21:34 +0430

    error handling for update

--- frontend/src/components/update.js
+++ frontend/src/components/update.js
@@ -12,6 +12,12 @@ function Update(prop) {
     const [email, setEmail] = useState(prop.data[0].fields.s_email);
     const [file, setFile] = useState();
     const [name, setName] = useState(prop.data[0].fields.s_name);
+    const [wrongAge, setWrongAge] = useState(false);
+    const [phoneConflict, setPhoneConflict] = useState(false);
+    const [emailConflict, setEmailConflict] = useState(false);
+    const [wrongEmail, setWrongEmail] = useState(false);
+    const [messagePanel, setMessagePanel] = useState(false);
+    const [typeOfError, setTypeOfError] = useState("");
     console.log(prop.data[0].pk);
 
     let formData = async function () {
@@ -23,25 +29,92 @@ function Update(prop) {
         formData.append("email", email);
         formData.append("phone", phone);
         formData.append("myFile", file);
-        formData.append("id", prop.data[0].pk);
 
-        const request = new Request("http://localhost:8000/update/");
-        fetch(request, {
-            headers: {
-                Head: document.cookie,
-            },
-            method: "POST",
-            body: formData,
-        })
-            .then((response) => response.json())
-            .then((res) => {
-                if (res.result == "true") {
-                    window.location.reload();
-                } else {
-                    history.push("/login");
-                }
-            });
+        if (name && fatherName && date && email && phone && file) {
+            const verifyRequest = new Request(
+                "http://localhost:8000/add/"
+            );
+            fetch(verifyRequest, {
+                headers: {
+                    Head: document.cookie,
+                },
+                method: "POST",
+                body: formData,
+            })
+                .then((response) => response.json())
+                .then((res) => {
+                    console.log(res);
+                    if (res.result === "wrong_age") {
+                        setWrongAge(true);
+                        setEmailConflict(false);
+                        setPhoneConflict(false);
+                        setWrongEmail(false);
+                    } else if (res.result === "phone") {
+                        setWrongAge(false);
+                        setEmailConflict(false);
+                        setPhoneConflict(true);
+                        setWrongEmail(false);
+                    } else if (res.result === "email") {
+                        setWrongAge(false);
+                        setPhoneConflict(false);
+                        setEmailConflict(true);
+                        setWrongEmail(false);
+                    } else if (res.result === "wrong_email") {
+                        setEmailConflict(false);
+                        setWrongAge(false);
+                        setPhoneConflict(false);
+                        setWrongEmail(true);
+                    } else if (res.result === 'true') {
+                        setEmailConflict(false);
+                        setWrongAge(false);
+                        setPhoneConflict(false);
+                        setWrongEmail(false);
+                        window.location.reload();
+                    } else {
+                            history.push("/login");
+                        }})
+        } else {
+            setTypeOfError("empty");
+            showAlert();
+        }
+
+        // console.log(file)
+        console.log(formData);
     };
+
+    let showAlert = () => {
+        setMessagePanel(!messagePanel);
+    };
+    let errorStyles = "text-xs text-red-500";
+
+    // let formData = async function () {
+    //     const formData = new FormData();
+    //     console.log(name);
+    //     formData.append("name", name);
+    //     formData.append("fatherName", fatherName);
+    //     formData.append("date", date);
+    //     formData.append("email", email);
+    //     formData.append("phone", phone);
+    //     formData.append("myFile", file);
+    //     formData.append("id", prop.data[0].pk);
+
+    //     const request = new Request("http://localhost:8000/update/");
+    //     fetch(request, {
+    //         headers: {
+    //             Head: document.cookie,
+    //         },
+    //         method: "POST",
+    //         body: formData,
+    //     })
+    //         .then((response) => response.json())
+    //         .then((res) => {
+    //             if (res.result == "true") {
+    //                 window.location.reload();
+    //             } else {
+    //                 history.push("/login");
+    //             }
+    //         });
+    // };
 
     return (
         <div className="w-1/3 bg-white rounded">
@@ -66,6 +139,11 @@ function Update(prop) {
                     onChange={(e) => setDate(e.target.value)}
                     className="w-full border-b border-green-400 py-1 my-2 px-3 focus:outline-none"
                 />
+                {wrongAge && (
+                    <p className={errorStyles}>
+                        *A student must be at least 5 years old
+                    </p>
+                )}
                 <input
                     type="text"
                     value={phone}
@@ -73,13 +151,21 @@ function Update(prop) {
                     className="w-full border-b border-green-400 py-1 my-2 px-3 focus:outline-none"
                     placeholder="Phone"
                 />
+                {phoneConflict && (
+                    <p className={errorStyles}>*This phone number is used</p>
+                )}
                 <input
                     type="email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     className="w-full border-b border-green-400 py-1 my-2 px-3 focus:outline-none"
                     placeholder="E-mail"
-                />
+                />{emailConflict && (
+                    <p className={errorStyles}>*This email is used</p>
+                )}
+                {wrongEmail && (
+                    <p className={errorStyles}>*Please, Enter a valid Email</p>
+                )}
                 <img
                     src={
                         "http://localhost:8000/studentImages/" +
